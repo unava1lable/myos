13f6163d9a6ce4b23b00ed4490f385f2204feff6
unava1lable <910654119@qq.com>
2022-06-02 15:16:35 +0800

    add bare metal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
 }
