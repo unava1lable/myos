0177cee7396f68c3cf82335144aed14c29c8bdef
unava1lable <910654119@qq.com>
2022-06-03 11:33:24 +0800

    implement basic vga text mode driver

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 #![no_std]
 #![no_main]
-use core::panic::PanicInfo;
+mod vga;
 
-static HELLO: &[u8] = b"Hello World!";
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    use core::fmt::Write;
+    vga::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga::WRITER.lock(), ", some number: {} {}", 42, 1.337).unwrap();
     loop {}
 }
 
