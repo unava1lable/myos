58df98d9a6819e9d2226a34c87eb4b3b96c4e64b
unava1lable <910654119@qq.com>
2022-06-05 13:11:04 +0800

    add hardware interrupt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,15 @@
 use crate::gdt;
+use crate::print;
 use crate::println;
 use lazy_static::lazy_static;
+use pic8259::ChainedPics;
+use spin;
 use x86_64::structures::idt::{ InterruptDescriptorTable, InterruptStackFrame };
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 lazy_static! {
 	static ref IDT: InterruptDescriptorTable = {
@@ -10,8 +18,25 @@ lazy_static! {
 		unsafe {
 			idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOULE_FAULT_IST_INDEX);
 		}
+		idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
 		idt
 	};
+}
+
+#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+pub enum InterruptIndex {
+	Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+	fn as_u8(self) -> u8 {
+		self as u8
+	}
+
+	fn as_usize(self) -> usize {
+		usize::from(self.as_u8())
+	}
 }
 
 pub fn init_idt() {
@@ -24,4 +49,11 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
 	panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+	print!(".");
+	unsafe {
+		PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use myos::print;
 use myos::println;
 
 #[no_mangle]
