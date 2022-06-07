f453d5ec0eaf009eb65b281ba455255181e156f3
unava1lable <910654119@qq.com>
2022-06-07 18:52:22 +0800

    implment mapping page

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,24 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use myos::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use myos::memory::BootInfoFrameAllocator;
+    use x86_64::structures::paging::Size4KiB;
+    use x86_64::{structures::paging::Page, VirtAddr};
 
     println!("Hello, World{}", "!");
     myos::init();
 
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
-
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
-
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
-
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    
+    let page: Page<Size4KiB> = Page::containing_address(VirtAddr::new(0xdeadbeef000));
+    memory::create_example_paging(page, &mut mapper, &mut frame_allocator);
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    
     #[cfg(test)]
     test_main();
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,46 @@
-use x86_64::{ structures::paging::PageTable, VirtAddr};
-use x86_64::structures::paging::OffsetPageTable;
+use bootloader::bootinfo::MemoryMap;
+use bootloader::bootinfo::MemoryRegionType;
+use x86_64::structures::paging::{PageTable, OffsetPageTable, Page, PhysFrame, Mapper, Size4KiB, FrameAllocator};
+use x86_64::{ PhysAddr, VirtAddr};
 
+// 一个FrameAllocator，它从bootloader的内存映射中返回一个可用的帧
+pub struct BootInfoFrameAllocator {
+	memory_map: &'static MemoryMap,
+	next: usize,
+}
+
+impl BootInfoFrameAllocator {
+	pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+		BootInfoFrameAllocator {
+			memory_map,
+			next: 0,
+		}
+	}
+
+	// 返回一个从mmap指定的可用帧的迭代器
+	fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+		// 获取mmap中的可用regions
+		let regions = self.memory_map.iter();
+		let usable_regions = regions
+			.filter(|r| r.region_type == MemoryRegionType::Usable);
+		// 将每个region映射成它的地址范围
+			let addr_ranges = usable_regions
+			.map(|r| r.range.start_addr()..r.range.end_addr());
+		// 转换为帧起始地址的迭代器
+		let frame_address = addr_ranges.flat_map(|r| r.step_by(4096));
+		frame_address.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+	}
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+	fn allocate_frame(&mut self) -> Option<PhysFrame> {
+		let frame = self.usable_frames().nth(self.next);
+		self.next += 1;
+		frame
+	}
+}
+
+// 返回4级页表的一个可变引用
 unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
 	use x86_64::registers::control::Cr3;
 
@@ -10,10 +50,23 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
 	let virt = physical_memory_offset + phys.as_u64();
 	let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
-	&mut *page_table_ptr
+	&mut *page_table_ptr	// unsafe
 }
 
+// 初始化一个新的OffsetPageTable
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
 	let level_4_table = active_level_4_table(physical_memory_offset);
 	OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+pub fn create_example_paging(page: Page, mapper: &mut OffsetPageTable, frame_allocator: &mut impl FrameAllocator<Size4KiB>) {
+	use x86_64::structures::paging::PageTableFlags as Flags;
+
+	let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+	let flags = Flags::PRESENT | Flags::WRITABLE;
+
+	let map_to_result = unsafe {
+		mapper.map_to(page, frame, flags, frame_allocator)
+	};
+	map_to_result.expect("map_to failed").flush();
 }
