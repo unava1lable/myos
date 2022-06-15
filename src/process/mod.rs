2fd29f0e8ec3e5f2125d5d62dd2da9b93562d8b1
unava1lable <910654119@qq.com>
2022-06-15 21:59:04 +0800

    implement switching context

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,1 +1,37 @@
 pub mod context;
+use alloc::alloc::{alloc, dealloc, Layout};
+use context::Context;
+
+const STACK_SIZE: usize = 0x80000;
+
+pub struct KernelStack(usize);
+
+impl KernelStack {
+	pub fn new() -> Self {
+		let bottom = unsafe {
+			alloc(Layout::from_size_align(STACK_SIZE, STACK_SIZE).unwrap())
+		} as usize;
+		Self(bottom)
+	}
+}
+
+impl Drop for KernelStack {
+	fn drop(&mut self) {
+		unsafe {
+			dealloc(self.0 as _, Layout::from_size_align(STACK_SIZE, STACK_SIZE).unwrap())
+		};
+	}
+}
+
+pub struct Thread {
+	pub context: Context,
+	pub kernel_stack: KernelStack,
+}
+
+impl Thread {
+	pub fn switch_to(&mut self, next: &mut Context) {
+		unsafe {
+			context::switch_to(&mut self.context, next)
+		};
+	}
+}
